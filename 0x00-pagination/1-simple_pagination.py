fc9bf5ad2418db4c54408bf1b8a7f08ab4d04780
islamhasib953 <islamhasib953@example.com>
2024-08-31 22:45:51 +0300

    1-simple_pagination.py

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+1-simple_pagination
+"""
+
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """function return a tuple of size two containing a
+    start index and an end index of page"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns the appropriate page of the dataset."""
+        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
+        assert (
+            isinstance(page_size, int) and page_size > 0
+        ), "Page size must be a positive integer."
+
+        start_index, end_index = index_range(page, page_size)
+
+        dataset = self.dataset()
+        return (
+            self.__dataset[start_index:end_index] if start_index < len(dataset) else []
+        )
