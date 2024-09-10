be656af868a61a6b8ac71456ca5fdf0129c5c787
islamhasib953 <islamhasib953@example.com>
2024-09-10 20:17:52 +0300

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,12 +4,14 @@
 
 from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
     "return page hello world"
-    return render_template('0-index.html')
+    return render_template("0-index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
